cedaf332b97daf74dbf2192f77a7c141ed9e419c
985892345 <guo985892345@foxmail.com>
2023-08-08 22:27:40 +0800

    Initial Commit

---
+++ ObjectiveCStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCStudy
+//
+//  Created by 郭祥瑞 on 2023/8/8.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
